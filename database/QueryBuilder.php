a11537328f256657c4a2eea70b9f4f86927b2b04
Mattias007 <mattiasmaasik@ametikool.ee>
2022-11-30 10:35:21 +0200

    asd

---
+++ database/QueryBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+class QueryBuilder{
+    
+    protected $pdo;
+
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function selectAll($tabel)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM {$tabel};");
+
+        $stmt -> execute();
+    
+        return $tasks = $stmt->fetchAll(PDO::FETCH_CLASS);
+    }
+}
--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 require_once 'Task.php';
 require_once 'function.php';
 require_once 'database/Connection.php';
-
+require_once 'database/QueryBuilder.php';
 
 $pdo = Connection::make();
-$tasks = fetchallTask($pdo);
 
 
+$query = new QueryBuilder($pdo);
+
+$tasks = $query->selectAll('tasks');
 
 
 require 'index.view.php';
