875b2473f30e932f9e0f46681889df2b010b8a5c
Mattias007 <mattiasmaasik@ametikool.ee>
2023-01-25 15:06:20 +0200

    s

--- views/about-culture.php
+++ views/about-culture.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/style.css">
-    <title>Document</title>
-</head>
-<body>
+<?php require_once 'partials/header.php' ?>
+
+
  <h1>about-culture</h1>
-</body>
+
+
+<?php require_once 'partials/footer.php'?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/style.css">
-    <title>Document</title>
-</head>
-<body>
+<?php require_once 'partials/header.php' ?>
     
     <?php require_once 'partials/nav.php'; ?>
 
@@ -29,5 +20,5 @@
         </li>
     <?php endforeach; ?>
 
-</body>
-</html>
+
+<?php require_once 'partials/footer.php' ?>
